c30b8c2c91bc8808dc22e25f953203eca7612502
joe-dm <130908030+joe-dm@users.noreply.github.com>
2023-05-14 23:15:25 +0100

    building gui
    further code re-structuring and further gui improvements

--- app/gui/console.py
+++ app/gui/console.py
@@ -1,18 +1,39 @@
+import PySide6.QtCore as QtCore
+import PySide6.QtGui as QtGui
 import PySide6.QtWidgets as QtWidgets
+
 from resources.config import ThemeConfig
+from util.logger import Logger
 
 class Console(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()          
-
-        # main widgets
+        
         self.text_edit = QtWidgets.QTextEdit()
 
         self.setup_ui()
+        Logger.set_gui_console(self)
 
     def setup_ui(self):       
         # setup layout
         layout = QtWidgets.QVBoxLayout(self)
+        layout.setContentsMargins(0,0,0,0)
         layout.addWidget(self.text_edit)
 
-        self.text_edit.setStyleSheet(f"background-color: {ThemeConfig.get_color('black-dark')};")
+        # set font
+        font = QtGui.QFont(ThemeConfig.font_console_name(), ThemeConfig.font_console_size())
+        self.text_edit.setFont(font)
+        # Disable text wrapping
+        self.text_edit.setWordWrapMode(QtGui.QTextOption.NoWrap)
+        # set background color
+        self.text_edit.setStyleSheet(f"background-color: {ThemeConfig.color_black_dark()}; border: none;")
+        # disable editing
+        self.text_edit.setReadOnly(True)
+
+
+    def append(self, message):
+        self.text_edit.append(message)
+        self.text_edit.ensureCursorVisible()
+        
+        scroll_bar = self.text_edit.horizontalScrollBar()
+        scroll_bar.setSliderPosition(scroll_bar.minimum())
--- app/gui/window.py
+++ app/gui/window.py
@@ -1,10 +1,13 @@
+import functools
+
 import PySide6.QtCore as QtCore
+import PySide6.QtGui as QtGui
 import PySide6.QtWidgets as QtWidgets
 
 from gui.sidebar import Sidebar
 from gui.console import Console
-from gui.page import PageStack, Page
-from resources.config import AppConfig
+from gui.page import PageStack
+from resources.config import AppConfig, ThemeConfig
 from util.logger import Logger
 
 class Window(QtWidgets.QMainWindow):
@@ -17,9 +20,14 @@ class Window(QtWidgets.QMainWindow):
         # set window properties
         self.setWindowTitle(AppConfig.name())
         self.setMinimumWidth(600)
-        self.setMinimumHeight(400)
+        self.setMinimumHeight(400)        
+        self.resize(800, 600)
         self.setCentralWidget(self.content)
         self.show()
+
+        # set icon
+        icon = QtGui.QIcon(ThemeConfig.logo())
+        self.setWindowIcon(icon)
 
 
 class Content(QtWidgets.QWidget):
@@ -38,26 +46,32 @@ class Content(QtWidgets.QWidget):
         # create widget to hold sidebar and pages
         main_view = QtWidgets.QWidget()
         # create main view (horizontal) layout, add sidebar and pages to it
-        main_view_layout = QtWidgets.QHBoxLayout(main_view)
+        main_view_layout = QtWidgets.QHBoxLayout(main_view)       
         main_view_layout.addWidget(self.sidebar)
         main_view_layout.addWidget(self.page_stack)
 
         # create vertical splitter
         vertical_splitter = QtWidgets.QSplitter(QtCore.Qt.Vertical)
         vertical_splitter.addWidget(main_view)
-        vertical_splitter.addWidget(self.console)
-        vertical_splitter.setSizes([300, 100])
+        vertical_splitter.addWidget(self.console)        
+        vertical_splitter.setSizes([350, 150])        
 
         # set content layout, add splitter to it
-        self.setLayout(QtWidgets.QVBoxLayout(self))
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.setContentsMargins(0,0,0,0)        
+        self.setLayout(layout)
         self.layout().addWidget(vertical_splitter)
 
     def setup_connections(self):
-        self.sidebar.button_dashboard.clicked.connect(
-            lambda: self.switch_page(self.page_stack.page_dashboard))
-        self.sidebar.button_cpu.clicked.connect(
-            lambda: self.switch_page(self.page_stack.page_cpu))
+        sidebar_buttons = [
+            (self.sidebar.button_dashboard, self.page_stack.page_dashboard),
+            (self.sidebar.button_cpu, self.page_stack.page_cpu)
+        ]
+        for button, page in sidebar_buttons:
+            button.button_icon.clicked.connect(functools.partial(self.switch_page, page=page))
+            button.button_text.clicked.connect(functools.partial(self.switch_page, page=page))
 
+    
     def switch_page(self, page):
         self.page_stack.page_stack_layout.setCurrentWidget(page)
-        Logger.log(f'Switched to page {page}', 'debug')
+        Logger.log(f'Switched to page "{page.label_title.text()}"', 'debug')
