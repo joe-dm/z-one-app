611888c01b5465d4b09355ed96dce0d5e88c54f0
joe-dm <130908030+joe-dm@users.noreply.github.com>
2023-05-21 00:04:07 +0100

    Enabled console colours

--- app/gui/console.py
+++ app/gui/console.py
@@ -26,12 +26,52 @@ class Console(QtWidgets.QWidget):
         self.text_edit.setWordWrapMode(QtGui.QTextOption.NoWrap)
         # set colors
         self.text_edit.setStyleSheet(
-            f"background-color: {ThemeConfig.Color.black_dark}; color: {ThemeConfig.Color.secondary}; border: none;")
+            f"background-color: {ThemeConfig.Color.black_dark}; border: none;")
         # disable editing
         self.text_edit.setReadOnly(True)
 
-    def append(self, message):
-        self.text_edit.append(message)
+    def append(self, message, flag):
+        full_message = f"{ThemeConfig.console_flags.get(flag, '')}{message}"
+
+        # Set message color
+        color = ThemeConfig.Color.white
+        if flag == 'operation':
+            color = ThemeConfig.Color.secondary
+        elif flag == 'debug':
+            color = ThemeConfig.Color.gray_dark
+        elif flag == 'warning':
+            color = ThemeConfig.Color.yellow
+        elif flag == 'error':
+            color = ThemeConfig.Color.red
+
+        # Get current cursor position
+        cursor = self.text_edit.textCursor()
+        cursor.movePosition(QtGui.QTextCursor.End)
+
+        # Insert the message with the desired color
+        cursor.insertHtml(f"<span style='color: {color};'>{full_message}</span>")
+        self.text_edit.setTextCursor(cursor)
+
+        # Move to the next line
+        cursor.insertText("\n")
+
+        # Scroll to the bottom and ensure cursor visibility
+        self.text_edit.ensureCursorVisible()
+
+        scroll_bar = self.text_edit.horizontalScrollBar()
+        scroll_bar.setSliderPosition(scroll_bar.minimum())
+
+
+    def append_old(self, message, flag):
+        full_message = f"{ThemeConfig.console_flags.get(flag, '')}{message}"
+
+        # set message color
+        color = ThemeConfig.Color.white
+        if flag == 'debug':
+            color = ThemeConfig.Color.green                    
+        self.text_edit.setStyleSheet(f"color: {color};")
+
+        self.text_edit.append(full_message)
         self.text_edit.ensureCursorVisible()
         
         scroll_bar = self.text_edit.horizontalScrollBar()
